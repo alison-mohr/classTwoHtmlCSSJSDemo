244003157848370a70391747f3abe761b1531d40
Alison Mohr <alison@newbo.co>
2020-06-16 23:22:02 -0500

    add java, html, css, reset file

---
+++ js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+//first thing to do
+console.log('This file is connected');
+
+
+//prompt our user for input data
+var userName = prompt('What is your name?');
+console.log(userName);
+
+//Greet the user
+alert('Hello,' + userName);
+
+
+//convert the input to lowercase
+
+
+
+
+//validate the input using conditional logic using strings
+
