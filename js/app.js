327216297ea2583c013eb8bd5953ccaad2f93783
Alison Mohr <alison@newbo.co>
2020-06-17 05:49:23 -0500

    add js

--- js/app.js
+++ js/app.js
@@ -10,11 +10,32 @@ console.log(userName);
 //Greet the user
 alert('Hello,' + userName);
 
-
 //convert the input to lowercase
-
-
+var lowerCaseName = userName.toLowerCase();
+console.log(lowerCaseName);
 
 
 //validate the input using conditional logic using strings
+if(lowerCaseName === 'craig' || lowerCaseName === 'bob') {
+  alert('Welcome back to my page, ' + userName);
+} else {
+  alert('Welcome new user to my page');
+}
 
+//logical operator
+var myBooleanValueTrue = false;
+var anotherBooleanValueFalse = false;
+var thirdBooleanValueTrue = true;
+
+if (myBooleanValueTrue && thirdBooleanValueTrue){
+  alert('The && will run if both are true');
+  console.log('both values were true');
+} else if(myBooleanValueTrue || anotherBooleanValueFalse){
+  alert('The || will run if one of the values was true');
+  console.log('one of the values was true');
+} else if(thirdBooleanValueTrue){
+  alert('the 3rd value was true, so this will run');
+  console.log('thirdBooleanValueTrue was true');
+} else {
+  console.log('There were no true values.');
+}
